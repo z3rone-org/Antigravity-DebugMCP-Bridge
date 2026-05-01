8efc266c2b45a76d66e70abc190a095aeb39669e
Falk B. Schimweg <git@falk.schimweg.de>
2026-05-01 13:57:41 +0200

    add packages

--- bridge.ts
+++ bridge.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Copyright (c) Microsoft Corporation.
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
